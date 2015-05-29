4893c2d256f49cb21f41dd69a7e78e27692bdfb0
Gauthier C <gc@ac-lille.fr>
2015-05-29 15:53:08 +0200

    Add -i flag to show ip

--- hostnameDNS.go
+++ hostnameDNS.go
@@ -7,7 +7,7 @@ import (
 	"regexp"
 )
 
-func revers(ip4 string, fqdn bool, short bool, domain bool) {
+func revers(ip4 string, fqdn bool, short bool, domain bool, ip bool) {
 	Hostname, err := net.LookupAddr(ip4)
 	if err == nil {
 		switch {
@@ -17,6 +17,8 @@ func revers(ip4 string, fqdn bool, short bool, domain bool) {
 		case fqdn:
 			regE := regexp.MustCompile("^(.*)[.]$")
 			fmt.Printf("%s\n", regE.FindStringSubmatch(Hostname[0])[1])
+		case ip:
+			fmt.Printf("%s\n", ip4)
 		default:
 			regE := regexp.MustCompile("^([^.]*)")
 			fmt.Printf("%s\n", regE.FindStringSubmatch(Hostname[0])[1])
@@ -27,8 +29,9 @@ func revers(ip4 string, fqdn bool, short bool, domain bool) {
 func main() {
 
 	fqdnPtr := flag.Bool("f", false, "long host name (FQDN)")
-	shortPtr := flag.Bool("s", false, "short host name")
+	shortPtr := flag.Bool("s", false, "short host name (default)")
 	domainPtr := flag.Bool("d", false, "DNS domain name")
+	ipPtr := flag.Bool("i", false, "addresses for the host name")
 	flag.Parse()
 
 	regS := regexp.MustCompile("[0-9.]*")
@@ -38,7 +41,7 @@ func main() {
 			for _, addr := range addrs {
 				monip := regS.FindString(addr.String())
 				if monip != "127.0.0.1" {
-					revers(monip, *fqdnPtr, *shortPtr, *domainPtr)
+					revers(monip, *fqdnPtr, *shortPtr, *domainPtr, *ipPtr)
 				}
 			}
 		}
